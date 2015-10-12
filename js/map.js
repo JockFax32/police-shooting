25f8ef1ac2cd0afff9bcb953e07d2da545a887b1
iGuest <foxtjack@gmail.com>
2015-10-12 12:04:39 -0700

    Init Commit

--- js/map.js
+++ js/map.js
@@ -2,13 +2,11 @@
 var drawMap() = function() {
 
   // Create map and set view
- 
-
+    var map = L.map('your-div-container-name').setView([latitude, longitude], center)
   // Create a tile layer variable using the appropriate url
-
-
+    var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
   // Add the layer to your map
- 
+    layer.addTo(map)
 
   // Execute your function to get data
  
