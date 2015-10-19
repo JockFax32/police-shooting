be8bd8b7ab864c2ccd64b66a62d6903e85236b3b
JockFax32 <foxtjack@gmail.com>
2015-10-18 19:43:28 -0700

    Final

--- Readme.md
+++ Readme.md
@@ -1,3 +1,18 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/police-shooting).
+
+##### 1. What is the URL of this site, as hosted on your student webserver (i.e., students.washington.edu/<code>{uw-new-id}</code>/info343/<code>{project-name}</code>) #####
+> http://students.washington.edu/foxtjack/info343/police-shooting/
+
+##### 2. Did you receive any help from others (classmates, etc)? If so, please list who. #####
+> Yes Arvind
+
+##### 3. Did you complete any advanced extensions to this challenge? If so, what? #####
+> No
+
+##### 4. Approximately how many hours did it take you to complete this challenge? #####
+> 5-10 hours
+
+##### 5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
+> Took awhile to get the hang of Leaflet
--- js/map.js
+++ js/map.js
@@ -1,14 +1,16 @@
 
 var jQuery = function(){
 $('a').attr('target','_blank');
-// $('#mapinfo:contains("Blue")').css('color','#3369E8');
-
+$('#gred').css('color','#D50f25');
+$('#gblue').css('color','#3369E8');
+$('#ggray').css('color','#666666');
 }
+
 // Function to draw your map
 var drawMap = function() {
 
   // Create map and set view
-    var map = L.map('map').setView([40.00, -100.00], 4)
+  var map = L.map('map').setView([40.00, -100.00], 4)
   // Create a tile layer variable using the appropriate url
  var mapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -18,105 +20,122 @@ var drawMap = function() {
     getData(map);
   }
 
-  // Execute your function to get data
- 
 // Function for getting data
 var getData = function(map) {
-
-  // Execute an AJAX request to get the data in data/response.js
   $.ajax({  
     url: ('../../data/response.json'),
     type:"get",
     success: function(data){
-      console.log("Load was a success");
-      customBuild(map,data);
-      
+      customBuild(map,data);    
     }
-    
   });
-  // When your request is successful, call your customBuild function
 }
+
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(map,data) {
+  //layers
   var raceBlackLayerGroup = new L.LayerGroup([]);
   var raceWhiteLayerGroup = new L.LayerGroup([]);
+  var raceAsianLayerGroup = new L.LayerGroup([]);
+  var raceNativeLayerGroup = new L.LayerGroup([]);
+  var raceIslanderLayerGroup = new L.LayerGroup([]); 
   var raceUnknownLayerGroup = new L.LayerGroup([]);
-  // var armed = new L.LayerGroup([]);
+  //Circle marker attr
+  var circleColor=null; 
+  var circleFill= null;
+  //Nums for cross tab
+  var blackHit=0;
+  var blackKilled =0; 
+  var whiteHit = 0;
+  var whiteKilled = 0;
 
-    // Loop through the array to add layers 
-    for (var i=0; i < data.length; i++){
-      var armed = data[i]["Armed or Unarmed?"];
-      var race = data[i].Race;
-      var circleColor, circleFill;
-
-      if (data[i].Race == "Black or African American") {
-        circleColor='#3369E8'; // Google Blue
-        circleFill ="#3369E8";
-        // console.log("Race Black?: " + race);
-        circle.bindPopup(data[i].Race+
-          "</br>"+"<b>Name: </b>" + data[i]["Victim Name"]+
-          "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
-        circle.addTo(raceBlackLayerGroup);
-      }
-      else if (data[i].Race == "White"){
-        circleColor="#D50f25"; // Google Red
-        circleFill ="#D50f25";
-        circle.bindPopup(data[i].Race+
-          "</br>"+"<b>Name: </b>" + data[i]["Victim Name"]+
-          "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
-        circle.addTo(raceWhiteLayerGroup);
-      }else if (data[i].Race !="White" || data[i].Race !="Black or African American"){
-        circleColor = "#666666"; //Google Gray 
-        circleFill= "666666";
-        // circle.bindPopup(data[i].Race);
-        // circle.addTo(raceUnknownLayerGroup);
-      }
-
-      var circle = new L.circleMarker([data[i].lat,data[i].lng],{
-        color:circleColor,
-        fillColor:circleFill,
+  // Loop through the array to add layers 
+  for (var i=0; i < data.length; i++){
+    var armed = data[i]["Armed or Unarmed?"];
+    var circle = L.circleMarker([data[i].lat,data[i].lng],{
         radius: 10,
-      });
+    });
+  
+  //Create colored circles
+  if (armed == "Unarmed"){
+    circleColor='#3369E8'; // Google Blue
+    circleFill ="#3369E8";
+    }
+    else if (armed == "Armed"){
+      circleColor="#D50f25"; // Google Red
+      circleFill ="#D50f25";
+      }
+    else if (armed=="Unknown" || armed==-1){
+    circleColor="#666666"; // Google Gray
+    circleFill ="#666666";
+      }
+     
+    // Create popup tags on markers
+  if (data[i].Race == "Black or African American") {
+    circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+      "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);  
+          // Tally for cross tab 
+        if(data[i]["Hit or Killed?"]=="Killed"){
+            blackKilled++;
+          }
+          else if (data[i]["Hit or Killed?"]=="Hit"){
+            blackHit++;
+          }
+    circle.addTo(raceBlackLayerGroup);
+    }
+    else if (data[i].Race == "White"){
+      circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+        "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
+          // Tally for cross tab 
+        if(data[i]["Hit or Killed?"]=="Killed"){
+            whiteKilled++;
+          }
+          else if (data[i]["Hit or Killed?"]=="Hit"){
+            whiteHit++;
+        }
+      circle.addTo(raceWhiteLayerGroup);
+      }
+    else if (data[i].Race == "Asian"){
+      circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+        "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
+      circle.addTo(raceAsianLayerGroup);
+      }
+    else if (data[i].Race =="American Indian or Alaska Native"){
+      circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+        "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
+      circle.addTo(raceNativeLayerGroup);
+      }
+    else if (data[i].Race=="Native Hawaiian or Other Pacific Islander"){
+      circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+         "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
+      circle.addTo(raceNativeLayerGroup);
+      }
+    else if (data[i].Race == "Unknown"|| data[i].Race ==-1){
+      circle.bindPopup("<b>Name: </b>" + data[i]["Victim Name"]+
+        "</br>"+"<b>Age: </b>" + data[i]["Victim's Age"]);
+      circle.addTo(raceUnknownLayerGroup);
+      }
+    circle.setStyle( {color:circleColor, fillColor:circleFill});
     }
 
-   // Add Layers
+   // Add Layers to map
    var raceLayers ={
     "Black": raceBlackLayerGroup,
     "White": raceWhiteLayerGroup,
+    "Asian": raceAsianLayerGroup,
+    "American Indian or Alaska Native": raceNativeLayerGroup,
+    "Native Hawaiian or Pacific Islander": raceIslanderLayerGroup,
     "Unidentified": raceUnknownLayerGroup,
-
    }  
+    // Add layers to control
     L.control.layers(null,raceLayers).addTo(map);
     
+    //Add values to table
+    $("#blackKilled").html(blackKilled);
+    $("#blackHit").html(blackHit);
+    $("#whiteHit").html(whiteHit);
+    $("#whiteKilled").html(whiteKilled);
 
-  // Once layers are on the map, add a leaflet controller that shows/hides layers
-  
-}
-
-
-
-
-
-
-
-
-// http://stackoverflow.com/questions/17371039/how-to-add-markers-bulk-in-leaflet
-
-// Objects in the JSON file:
-// {"Date Searched":"10/15/1986",
-// "State":"AZ - Arizona",
-// "County":"maricopa",
-// "City":"Phoenix",
-// "Agency Name":"phoenix police",
-// "Victim Name":"David Valenzuela",
-// "Victim's Age":"24",
-// "Victim's Gender":"Male",
-// "Hit or Killed?":"Killed",
-// "Armed or Unarmed?":"Armed",
-// "Weapon":"Handgun",
-// "Summary":"Man in wheelchair threatened two officers with gun. they shot and killed him",
-// "Source Link":"http://www.fox10phoenix.com/story/18078667/man-in-wheelchair-shot-to-death-by-police",
-// "lat":33.4467681,
-// "lng":-112.0756724}
+ } 
 
 
